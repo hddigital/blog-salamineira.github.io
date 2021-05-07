56ac421b975cbe7bdedb932f191b085fb6651044
hddigital <83534552+hddigital@users.noreply.github.com>
2021-05-07 07:41:51 -0300

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -5,11 +5,34 @@ import BlogRoll from '../../components/BlogRoll'
 
 export default class BlogIndexPage extends React.Component {
   render() {
-    return (  <section className="section">
-            <div className="container">
+    return (
+      <Layout>
+        <div
+          className="full-width-image-container margin-top-0"
+          style={{
+            backgroundImage: `url('/img/fundo-mega-de-hoje-header-megadehoje.png')`,
+          }}
+        >
+          <h1
+            className="has-text-weight-bold is-size-1"
+            style={{
+              boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
+              backgroundColor: '#f40',
+              color: 'white',
+              padding: '1rem',
+            }}
+          >
+            Últimas Notícias
+          </h1>
+        </div>
+        <section className="section">
+          <div className="container">
             <div className="content">
-              <BlogRoll />           
-       
-            )
+              <BlogRoll />
+            </div>
+          </div>
+        </section>
+      </Layout>
+    )
   }
 }
